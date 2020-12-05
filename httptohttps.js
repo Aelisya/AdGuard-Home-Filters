3df6fede99f02ba8f8fbba4522ec11f2a437ba7a
Michael B <bevode@aelisya.net>
2020-12-06 00:52:42 +0100

    Update httptohttps.js

--- httptohttps.js
+++ httptohttps.js
@@ -1,3 +1,11 @@
+// ==UserScript==
+// @name         Secure Redirect
+// @version      1.0
+// @description  A simple userscript that redirects pages to https:.
+// @author       Aelisya
+// @match        *
+// @grant        none
+// ==/UserScript==
 (function (w) {
     'use strict';
     var secureProtocol = 'https';
