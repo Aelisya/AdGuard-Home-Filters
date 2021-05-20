0e3264e2c415995dda90783140a03b6c191507af
Michael B <security@bacq.pro>
2021-05-20 04:13:37 +0200

    Update Hardallows.adb

--- AdGuard-Home/Hardallows.adb
+++ AdGuard-Home/Hardallows.adb
@@ -1,45 +1,14 @@
 ! Title: Macqael Hardallow
 ! Description: Filter composed of several Allowed domain unlocked with Adblock Plus Syntax for DNS.
-! Version: 4.4
+! Version: 4.5
 ! Homepage: https://github.com/macqael/AdGuard-Home-Filters
 ! License: https://github.com/macqael/AdGuard-Home-Filters/blob/main/LICENSE
-! Last modified: 2021-05-16
+! Last modified: 2021-05-20
 @@||feedproxy.google.com^
 @@||adblock-tester.com^
-@@||mega.io^
-@@||youtu.be^
-@@||consent.youtube.com^
-@@||consent.google.be^
-@@||consent.google.com^
-@@||cse.google.com^
-@@||mega.co.nz^
-@@||mega.nz^
-@@||googlevideo.com^
-@@||gvt1.com^
-@@||storage.googleapis.com^
-@@||googlehosted.com^
-@@||mtalk.google.com^
-@@||fonts.googleapis.com^
-@@||www.googleapis.com^
-@@||ajax.googleapis.com^
-@@||youtube-nocookie.com^
-@@||ggpht.com^
-@@||ytimg.com^
-@@||www.google.com^
-@@||www.gstatic.com^
-@@||googleusercontent.com^
-@@||fonts.gstatic.com^
-@@||pki-goog.l.google.com^
 @@||tracking.epicgames.com^
 @@||mail.levif.be^
-@@||crl.pki.goog^
-@@||ocsp.pki.goog^
-@@||edge-chat.facebook.com^
 @@||l.facebook.com^
-@@||www.facebook.com^
-@@||fbcdn.net^
-@@||fbcdn.com^
-@@||9hentai.ru^
 @@||mjt.lu^
 @@||ac.duckduckgo.com^
 @@||links.duckduckgo.com^
@@ -50,15 +19,3 @@
 @@||roularta.be^
 @@||mlsend.com^
 @@||lencr.org^
-@@||phncdn.com^
-@@||pornhub.com^
-@@||lewdpatcher.com^
-@@||hanime.tv^
-@@||f95zone.com^
-@@||f95zone.to^
-@@|star-mini.c10r.facebook.com
-@@|login.facebook.com^
-@@|graph.facebook.com^
-@@|star.c10r.facebook.com^
-@@|youtube.com^
-@@|www.youtube.com^
